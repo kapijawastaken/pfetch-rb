24f49d6db8ef7968ddcfe197a8d39710649ebf23
Gabriell Simic <gabriellsimic5@gmail.com>
2025-05-14 13:36:15 +0200

    fixed days (hopefully)

--- rubyfetch.rb
+++ rubyfetch.rb
@@ -3,7 +3,7 @@ logos = File.expand_path(File.read(File.expand_path("~/.config/rubyfetch/config"
 user = `whoami`.strip+"@"+`hostname`.strip+"\n--------------------"
 kernel = `uname -r`
 shell = ENV["SHELL"].gsub(/^.+\//, "")
-uptime = `uptime`.gsub(/^.*up\s+/, "").gsub(/,.*/, "").gsub(":", " hours, ").gsub(/\b0/, "").strip.gsub(/^hours,/, "").strip.gsub("min", "").gsub(" h", "").strip+" mins"
+uptime = `uptime`.gsub(/^.*up\s+/, "").gsub(/,.*/, "").gsub(":", " hours, ").gsub(/\b0/, "").strip.gsub(/^hours,/, "").strip.gsub("min", "").gsub(" h", "").strip.gsub("days", "").strip+" mins"
 # distro
 if `uname -a`.strip.include?("Android")
   distro = "Android"
